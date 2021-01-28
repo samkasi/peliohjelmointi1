d38042b11a9fd9b081efa9eaad0d7ef5a35a594f
samkasi <77836981+samkasi@users.noreply.github.com>
2021-01-28 13:27:00 +0200

    testi hyppy

---
+++ Peliohjelmointi 1/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Sami Kaski 26.1.2021
+    public float jumpPower = 10.0f;
+    Rigidbody2D myRigidbody;
+    public bool isGrounded = false;
+    void Start()
+    {
+        myRigidbody = transform.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidbody.AddForce(Vector3.up * (jumpPower * myRigidbody.gravityScale * 20.0f));
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
