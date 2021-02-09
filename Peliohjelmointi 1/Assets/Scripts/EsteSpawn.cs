8a7d1915815aae7d5917d3d489e41d10adb73e10
samkasi <77836981+samkasi@users.noreply.github.com>
2021-02-09 14:15:42 +0200

    Esteliikkuminen

---
+++ Peliohjelmointi 1/Assets/Scripts/EsteSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Start is called before the first frame update
+public class EsteSpawn : MonoBehaviour
+{
+    public float scrollSpeed = 5.0f;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        GameObject currentChild;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+        }
+        
+        
+    }
+
+    void ScrollChallenge(GameObject currentChallenge)
+    {
+        currentChallenge.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+    }
+}
