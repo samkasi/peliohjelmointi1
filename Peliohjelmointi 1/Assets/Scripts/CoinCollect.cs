b695b1f7bdc3b03a4988f2667b44c93a5acbc5cc
TatuJutila <tatujutila@kamk.fi>
2021-02-23 12:32:38 +0200

    Added Score System

---
+++ Peliohjelmointi 1/Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        ScoreSystem.theScore += 1;
+        Destroy(gameObject);
+    }
+}
